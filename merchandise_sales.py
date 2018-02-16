112f9d95590b1bf33ea26623c73ffe0e73230b4a
Dilli <me@email.com>
2018-02-16 15:20:25 -0600

    Modified the function that adds data to tables

--- merchandise_sales.py
+++ merchandise_sales.py
@@ -3,34 +3,38 @@ import traceback
 import datetime
 import dataManagement
 
-
-
-
 def handle_choice(choice):
     if choice == '1':
-        add_data()
+        add_data_to_event()
     elif choice == '2':
+        add_data_to_item()
+    elif choice == '3':
         display_data()
     elif choice == '5':
         delete_data()
     else:
         print('Please enter the correct choice.')
 
-def add_data():
-    place =input('Enter the venue: ')
-    item_name = input('Enter the item name: ')
-    item_price = input('Enter the item price: ')
-    no_of_items = input('Enter the number of items: ')
+def add_data_to_event():
+    place =ui.input_event()
+
     today = datetime.datetime.now()
     dataManagement.create_event_table()
-    dataManagement.create_item_table()
+
     dataManagement.create_sales_table()
     dataManagement.add_to_event(None,today,place)
-    event_id = dataManagement.last_rowid()
-    dataManagement.add_to_item(None,event_id, item_name, item_price, no_of_items)
-    item_id = dataManagement.sales_lastRowID()
-    dataManagement.add_to_sales(None,item_id,no_of_items)
 
+    #item_id = dataManagement.sales_lastRowID()
+    #dataManagement.add_to_sales(None,item_id,no_of_items)
+
+def add_data_to_item():
+    item_name = input('Enter the item name: ')
+    event_id = input('Enter the event id : ')
+    item_price = input('Enter the item price: ')
+    no_of_items = input('Enter the number of items: ')
+    dataManagement.create_item_table()
+    #event_id = dataManagement.last_rowid()
+    dataManagement.add_to_item(None,event_id, item_name, item_price, no_of_items)
 def display_data():
     dataManagement.show_Events_data()
     print()
@@ -41,6 +45,7 @@ def display_data():
 def delete_data():
     dataManagement.delete_item_data()
     dataManagement.delete_event_data()
+    dataManagement.delete_sale_data()
 
 def main():
     quit = 'q'
--- ui.py
+++ ui.py
@@ -1,12 +1,17 @@
 '''display menu of choices'''
 def get_menu_choices():
     print('''
-    1. Enter data
-    2. Show total items sold
-    3. Update data
-    4. Search data
-    5. Delete data
-    6. Save data
+    1. Enter data to Events table
+    2. Enter data to Items table
+    3. Enter data to sales table
+    4. Show total items sold
+    5. Update data
+    6. Search data
+    7. Delete data
+    8. Save data
     q. Quit
     ''')
     return input('Enter choice: ')
+
+def input_event():
+    return input('Enter venue name: ')
